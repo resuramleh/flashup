24b950a72f6b0266e03406696e806afaa4a6862f
resuramleh <marco@hellwiedunkel.de>
2013-07-15 20:15:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content=fs.readFileSync("index.html","utf-8");
 
 app.get('/', function(request, response) {
-    response.send(content);
+    response.send(fs.readFileDync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
