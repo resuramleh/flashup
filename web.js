20ba55c958db824222c846b09dbccf0b1db8f2a9
resuramleh <marco@hellwiedunkel.de>
2013-07-15 21:18:10 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var fs = require('fs');
-var port = process.env.PORT || 5000;
 var app = express.createServer(express.logger());
 
 var buff = new BUFFER(fs.readFileSync('index.html'));
@@ -9,6 +8,8 @@ var content = buff.toString('utf-8')
 app.get('/', function(request, response) {
     response.send(content);
 });
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
     console.log("Listening on " + port);
